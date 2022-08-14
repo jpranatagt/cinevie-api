adedfa7acb318cd0eb88faea2c672a49efd6bad6
Jepri Pranata Ginting <jpranata.ginting@gmail.com>
2022-08-15 00:05:48 +0700

    create reusable readIDParam helper method

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,27 +3,21 @@ package main
 import (
   "fmt"
   "net/http"
-  "strconv"
-
-  "github.com/julienschmidt/httprouter"
 )
 
 // POST method with /v1/movies endpoint
-func (app *application) createMovieHandler(writer http.ResponseWriter, response *http.Request) {
-  fmt.Fprintln(writer, "create a new movie")
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "create a new movie")
 }
 
 // GET method with /v1/movies/:id endpoint
-func (app *application) showMovieHandler(writer http.ResponseWriter, response *http.Request) {
-  // retrieve a slice containing URL parameter
-  params := httprouter.ParamsFromContext(response.Context())
-
-  // get id using ByName and return 404 Not Found response if id is invalid
-  id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// use readIDParam helper method
+	id, err := app.readIDParam(r)
   if err != nil || id < 1 {
-    http.NotFound(writer, response)
+    http.NotFound(w, r)
     return
   }
 
-  fmt.Fprintf(writer, "show the details of movie %d\n", id)
+  fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
