5f547c0c1f9fb819599d58d6d4570692064219d1
Jepri Pranata Ginting <jpranata.ginting@gmail.com>
2022-08-15 08:19:52 +0700

    implement JSON encoding helper method

--- cmd/api/helper.go
+++ cmd/api/helper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "encoding/json"
   "errors"
   "net/http"
   "strconv"
@@ -18,4 +19,27 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
   }
 
   return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+  // encode the data into JSON return error if there was one
+  js, err := json.Marshal(data)
+  if err != nil {
+    return err
+  }
+
+  // append new line for terminal view
+  js = append(js, '\n')
+
+  // loop header map and add each header to http.ResponseWriter header
+  for key, value := range headers {
+    w.Header()[key] = value
+  }
+
+  // Add the content type header, write status code and JSON response
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(status)
+  w.Write(js)
+
+  return nil
 }
--- cmd/api/status.go
+++ cmd/api/status.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-  "fmt"
   "net/http"
 )
 
 // response method with status, operating environment and version
 func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
-  js := `{"status": "available", "environment": %q, "version": %q}`
-  js = fmt.Sprintf(js, app.config.env, version)
+  // a map which holds the response information
+  data := map[string]string {
+    "status": "available",
+    "environment": app.config.env,
+    "version": version,
+  }
 
-  // response header as json
-  w.Header().Set("Content-Type", "application/json")
-
-  // http response body
-  w.Write([]byte(js))
+  err := app.writeJSON(w, http.StatusOK, data, nil)
+  if err != nil {
+    app.logger.Println(err)
+    http.Error(w, "The server encontered a problem and could not process your request.", http.StatusInternalServerError)
+  }
 }
