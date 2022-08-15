45c130591da049cf9b95e11e1fbf023e2f50bc39
Jepri Pranata Ginting <jpranata.ginting@gmail.com>
2022-08-15 08:15:38 +0700

    sending a fixed JSON format

--- cmd/api/status.go
+++ cmd/api/status.go
@@ -5,8 +5,14 @@ import (
   "net/http"
 )
 
+// response method with status, operating environment and version
 func (app *application) statusHandler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "status: available")
-  fmt.Fprintf(w, "environment: %s\n", app.config.env)
-  fmt.Fprintf(w, "version: %s\n", version)
+  js := `{"status": "available", "environment": %q, "version": %q}`
+  js = fmt.Sprintf(js, app.config.env, version)
+
+  // response header as json
+  w.Header().Set("Content-Type", "application/json")
+
+  // http response body
+  w.Write([]byte(js))
 }
