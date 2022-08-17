e40489884405277b471194137ebedb7e43e518c5
Jepri Pranata Ginting <jpranata.ginting@gmail.com>
2022-08-18 02:25:07 +0700

    generate authentication token for a valid email and password request

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -22,20 +22,24 @@ func (app *application) routes() http.Handler {
 
   // register relevant methods, URL patterns and handler functions for the endpoints
   // using HandlerFunc method
+
+	// application condition and info
   router.HandlerFunc(http.MethodGet, "/v1/status", app.statusHandler)
+
+	// movies
   router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
   router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
   router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
   router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 
+	//users
   router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
-	// use PUT method since it's idempotent after activation succeed
-  // the subsequent requests would be resulted in error
-  // or the state of application is not change after first succeed activation
   router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// tokens
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/activation", app.createActivationTokenHandler)
+router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	return app.recoverPanic(app.rateLimit(router))
 }
--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -80,3 +80,71 @@ func (app *application) createActivationTokenHandler(w http.ResponseWriter, r *h
     app.serverErrorResponse(w, r, err)
   }
 }
+
+func (app *application) createAuthenticationTokenHandler(w http.ResponseWriter, r *http.Request) {
+  // parse the email and password from the request body
+  var input struct {
+    Email     string  `json:"email"`
+    Password  string  `json:"password"`
+  }
+
+  err := app.readJSON(w, r, &input) // parse and assign them to input struct
+  if err != nil {
+    app.badRequestResponse(w, r, err)
+
+    return
+  }
+
+  // validate the email and password
+  v := validator.New()
+  data.ValidateEmail(v, input.Email)
+  data.ValidatePasswordPlaintext(v, input.Password)
+
+  if !v.Valid() {
+    app.failedValidationResponse(w, r, v.Errors)
+
+    return
+  }
+
+  // if not matching return invalidCrendentialsResponse with 401 code
+  user, err := app.models.Users.GetByEmail(input.Email)
+  if err != nil {
+    switch {
+      case errors.Is(err, data.ErrRecordNotFound):
+        app.invalidCredentialsResponse(w, r)
+      default:
+        app.serverErrorResponse(w, r, err)
+    }
+
+  return
+  }
+
+  // check if password matches
+  match, err := user.Password.Matches(input.Password)
+  if err != nil {
+    app.serverErrorResponse(w, r, err)
+
+    return
+  }
+
+  // if the password didn't match then call invalidCredentialsResponse again
+  if !match {
+    app.invalidCredentialsResponse(w, r)
+
+    return
+  }
+
+  // generate a new token with the 24 hours expiry and the scope 'authentication'
+  token, err := app.models.Tokens.New(user.ID, 3 * time.Hour, data.ScopeAuthentication)
+  if err != nil {
+    app.serverErrorResponse(w, r, err)
+    return
+  }
+
+  // encode the token to JSON and send it in the response along with a 201 status code
+  err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
+  if err != nil {
+    app.serverErrorResponse(w, r, err)
+  }
+}
+
